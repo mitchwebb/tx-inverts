7c64b7e22c9b2a3e9950a56452930cb356c0f66c
Webb <mdw2685@eid.utexas.edu>
2026-06-10 12:58:16 -0500

    update to get_latest_record_date

--- backend/data_util/gbif/get_latest_record_date.py
+++ backend/data_util/gbif/get_latest_record_date.py
@@ -1,7 +1,9 @@
-from datetime import date
+from datetime import datetime, date
 from backend.data_util.execute_psql_query import execute_psql_query
 from psycopg import AsyncConnection, sql
 from typing import Literal
+
+from backend.db.schema.gbif_observations import GBIF_OBSERVATIONS_TABLE
 
 
 async def get_latest_record_date(conn: AsyncConnection, param: Literal['last_interpreted', 'modified']) -> date | None:
@@ -16,16 +18,23 @@ async def get_latest_record_date(conn: AsyncConnection, param: Literal['last_int
         datetime | None: the latest date, or None if no data (or unreasonable date)
     """
 
-    query = sql.SQL("""
-        SELECT MAX({param}) FROM gbif_observations
-    """).format(param=sql.Identifier(param))
+    now = datetime.now()
+
+    query = sql.SQL('''
+        SELECT MAX({param}) FROM {observations_table}
+        WHERE {param} <= {now}
+    ''').format(
+        param=sql.Identifier(param),
+        observations_table=sql.Identifier(GBIF_OBSERVATIONS_TABLE.name),
+        now=sql.Literal(now.isoformat())
+    )
 
     result = await execute_psql_query(conn, query, fetch='one')
 
     if result and result[0]:
-        latest_date = result[0].date()
-        today = date.today()
+        latest = result[0]
+        latest_date = latest.date() if isinstance(latest, datetime) else latest
 
-        return latest_date if latest_date <= today else None
+        return latest_date if latest_date <= now.date() else None
 
     return None
