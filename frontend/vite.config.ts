7e6cd62004531fb2aa5be073500ab2570a3a9fea
Webb <mdw2685@eid.utexas.edu>
2026-09-23 15:10:35 -0500

    switching to vite loadEnv

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,33 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-let proxy = process.env.VITE_API_PROXY || 'http://localhost:8000';
+import { loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [svelte()],
-    server: {
-        proxy: {
-            '/server': {
-                target: proxy, // Backend server
-                changeOrigin: true, // Needed for virtual host-based backends
-                secure: false, // Disable SSL verification if using self-signed certs
-                rewrite: (path) => path.replace(/^\/server/, ''), // Optional path rewrite
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const proxy = env.VITE_API_PROXY || 'https://localhost:8000';
+
+    return {
+        plugins: [svelte()],
+        server: {
+            proxy: {
+                '/server': {
+                    target: proxy, // Backend server
+                    changeOrigin: true, // Needed for virtual host-based backends
+                    secure: false, // Disable SSL verification if using self-signed certs
+                    rewrite: (path) => path.replace(/^\/server/, ''), // Optional path rewrite
+                },
             },
         },
-    },
-    test: {
-        environment: 'jsdom',
-        exclude: ['**/node_modules/**', '**/dist/**', 'ui-tests/**'],
-    },
-    // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
-    resolve: process.env.VITEST
-        ? {
-              conditions: ['browser'],
-          }
-        : undefined,
+        test: {
+            environment: 'jsdom',
+            exclude: ['**/node_modules/**', '**/dist/**', 'ui-tests/**'],
+        },
+        // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+        resolve: process.env.VITEST
+            ? {
+                  conditions: ['browser'],
+              }
+            : undefined,
+    };
 });
