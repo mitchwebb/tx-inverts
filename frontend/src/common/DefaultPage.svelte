98c6ffb0692308a42cdb11fd6646d5975c291a85
Webb <mdw2685@eid.utexas.edu>
2026-09-23 16:24:55 -0500

    sidebar min-height

--- frontend/src/common/DefaultPage.svelte
+++ frontend/src/common/DefaultPage.svelte
@@ -46,6 +46,7 @@
         display: flex;
         flex-direction: column;
         max-width: 325px;
+        min-height: 100px;
     }
     .page-wrapper.overlay #default-sidebar-wrapper {
         background-color: var(--container-back);
